2a322ac72bfa59c0a3749761ce0f685889d2b21b
Traitet <traitet@gmail.com>
2020-07-20 22:05:41 +0700

    CREATE PROJECT

---
+++ lib/page/Ep10Page.dart
@@ -0,0 +1,40 @@
+
+//==================================================================
+// IMPORT 
+//==================================================================
+import 'package:flutter/material.dart';
+
+
+
+//==================================================================
+// MAIN CLASS
+//==================================================================
+class Ep10Page extends StatefulWidget {
+  @override
+  _Ep10PageState createState() => 
+  
+  
+  _Ep10PageState();
+}
+
+//==================================================================
+// STATE CLASS
+//==================================================================
+class _Ep10PageState extends State<Ep10Page> {
+//==================================================================
+// BUILD WIDGET
+//==================================================================  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('EP10 Page'),),
+      body: 
+      Center(child: Text('EP10 Page, 20 July 2020')),
+      // Container(
+      //   color: Colors.amber,
+      //   child: Text('Test'),
+      // )
+      
+    );
+  }
+}
